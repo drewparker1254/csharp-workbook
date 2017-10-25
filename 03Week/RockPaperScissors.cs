a3b81b0c994957b702b8a1fffad06eccb686ecf1
drewparker1254 <drewparker1254>
2017-10-24 20:16:25 -0500

    rock paper scissors

--- 03Week/RockPaperScissors.cs
+++ 03Week/RockPaperScissors.cs
@@ -9,11 +9,53 @@ public class Program
         Console.WriteLine("Enter hand 2:");
         string hand2 = Console.ReadLine().ToLower();
         Console.WriteLine(CompareHands(hand1, hand2));
+
+        CompareHands(hand1, hand2);
+
+        Console.ReadLine();
     }
     
     public static string CompareHands(string hand1, string hand2)
     {
+        //check for tie
+        if (hand1 == hand2)
+        {
+            return "It's a tie";
+        }
+
+        if(hand1 == "rock")
+        {
+            if(hand2 == "paper")
+            {
+                return "Hand2 Won!";
+
+            }
+
+            return "Hand1 Won1";
+
+        }
+
+        if (hand1 == "paper")
+        {
+            if(hand2 == "scissors")
+            {
+                return "Hand2 Won!";
+            }
+
+            return "Hand1 Won!";
+        }
+
+        if (hand1 == "scissors")
+        {
+            if (hand2 == "rock")
+            {
+                return "Hand2 Won!";
+            }
+
+            return "Hand1 Won!";
+        }
+
         // Your code here
-        return hand1 + " " + hand2;
+        return $"{hand1} played against {hand2}";
     }
 }
