d6f15218bc37c71e0f66d358d14636e1837112f3
drewparker1254 <drewparker1254>
2017-11-02 18:40:27 -0500

    still need to finish tower of hanoi

--- 04Week/TowersOfHanoi.cs
+++ 04Week/TowersOfHanoi.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public class Program
 {
@@ -12,28 +13,76 @@ public class Program
     
     public static void Main()
     {
-        printStacks();
-        // Your code here
+
+        // Loop thrugh methods using do {..} while()
+        // think when loop will end 
+        // think what methods go inside the code block
+
+        do {
+            PrintStacks();
+            //ask for start input - Console.WriteLine("...")
+            Console.WriteLine("Choose start pillar");
+            //capture that input using Console.ReadLine()
+           string start = Console.ReadLine();
+            //ask for finish input  using Console.ReadLine()
+            Console.WriteLine("Choose finish pillar");
+           string finish = Console.ReadLine();
+
+            //check if move legal
+
+            // only make move if move was legal
+
+        } while (!CheckForWin()); /// when this will stop
+        Console.ReadLine();
+
+
     }
     
-    public static bool GameOver()
+    public static bool CheckForWin()
     {
-        // Your code here;
+        // We need to count the elements on
+        // pillar "c"
+
+        if (stacks["c"].Count == 4)
+            return true;
+
         return false;
     }
     
-    public static void movePiece(string start, string finish)
+    public static void MovePiece(string start, string finish)
     {
         // Your code here
+
+       
+
+        stacks[start].Last();
+
+        int lastElement = stacks[start].Last();
+
+        stacks[finish].Add(lastElement);
+
+        stacks[start].Remove(lastElement);
     }
     
-    public static bool isLegal(string start, string finish)
+    public static bool IsLegal(string start, string finish)
     {
-        // Your code here
+        // check for empty pillar - finish(List)
+        if (stacks[finish].Count == 0)
+            return true;
+
+        // check for existing values on both pillars
+        int lastNumberFromStart = stacks[start].Last();
+        int lastNumberFromFinish = stacks[finish].Last();
+
+        if (lastNumberFromStart < lastNumberFromFinish)
+            return true;
+
+
+
         return false;
     }
     
-    public static void printStacks ()
+    public static void PrintStacks ()
     {
         string[] letters = new string[] {"a", "b", "c"};
         for( var i = 0; i < letters.Length; i++ )
