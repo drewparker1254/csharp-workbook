49e84aa5a9c514ff328d12083f857f34cadb75d5
drewparker1254 <drewparker1254>
2017-11-14 20:49:51 -0600

    Added Stack

--- 06Week/Stack.cs
+++ 06Week/Stack.cs
@@ -1,9 +1,87 @@
 using System;
+using System.Collections.Generic;
 
 public class Program
 {
 	public static void Main()
 	{
-		Console.WriteLine("Hello World");
-	}
+        // Instance of Stack Class
+        Stack myStack = new Stack();
+
+        try
+        {
+            myStack.Push("drew");
+            myStack.Push(112889);
+            myStack.Push(DateTime.Now);
+            myStack.Push(true);
+            myStack.Push(null); // exception 
+        }
+        catch(InvalidOperationException ex)
+        {
+            Console.WriteLine(ex.Message);
+            Console.WriteLine(ex.StackTrace);
+        }
+                       
+        Console.WriteLine(myStack.Pop());
+        Console.WriteLine(myStack.Pop());
+        Console.WriteLine(myStack.Pop());
+        Console.WriteLine(myStack.Pop());
+        
+
+       
+        Console.ReadLine();
+
+
+    }
+}
+
+
+
+public class Stack
+{
+    // Properties 
+    List<object> lifo { get; set; }
+
+
+    // Constructor
+
+        public Stack()
+        {
+        lifo = new List<object>();
+        }
+
+    // Methods
+
+    //push 
+    public void Push(object obj)   
+    {
+        if (obj == null)
+        {
+            throw new InvalidOperationException("Whoa! don't add null to the list.");
+        }
+
+        lifo.Add(obj);
+    }
+    //pop
+    public object Pop()
+    {
+        if(lifo.Count == 0)
+        {
+            throw new InvalidOperationException("The list is already empty!");
+        }
+
+        int indexOfLast = lifo.Count - 1;
+
+        object cloned = lifo[indexOfLast];
+
+        lifo.RemoveAt(indexOfLast);
+
+        return cloned;
+    }
+
+    //clear
+    public void Clear()
+    {
+        lifo.Clear();
+    }
 }
